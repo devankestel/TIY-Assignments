4364e4205cddf8771ca2c77635747ac19c02f9b0
Devan Kestel <devan.kestel@gmail.com>
2015-05-20 14:19:02 -0400

    Add server.rb and home.html
    Built simple server that loads home.html and includes ability to start
    and shutdown server.

---
+++ server.rb
@@ -0,0 +1,14 @@
+require "webrick"
+
+server = WEBrick::HTTPServer.new(:Port=>8000)
+
+server.mount_proc "/home" do |request, response|
+  response.body= File.read "home.html"
+end 
+
+server.mount_proc "/shutdown" do |request, response|
+  response.body = "Bye"
+  server.shutdown
+end
+
+server.start
