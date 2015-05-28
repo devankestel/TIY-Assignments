5d6150a9e3d54664e59cc62e5733ec2f89c05178
Devan Kestel <devan.kestel@gmail.com>
2015-05-27 21:11:32 -0400

    Basic server, class and ERB files created.
    Wrote server code to make sure server is working properly. Made a few
    ERB files to be read in as response.body. Made person.rb class file,
    currently empty.

---
+++ active_record_iron_roster/roster_server.rb
@@ -0,0 +1,22 @@
+require "webrick"
+require "./person"
+
+server = WEBrick::HTTPServer.new(:Port => 8000)
+
+server.mount_proc "/new_student_form" do |request, response|
+  template = ERB.new(File.read "new_student_form.html.erb")
+  response.body = template.result
+end
+
+server.mount_proc "/create_student" do |request, response|
+  @student = Person.create(request.query)
+  template = ERB.new(File.read "student.html.erb")
+  response.body = template.result
+end
+
+server.mount_proc "/shutdown" do |request, response|
+  response.body = "Deuces!"
+  server.shutdown
+end
+
+server.start
