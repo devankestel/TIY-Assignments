27f44ec46b99efa11f600382090e0b7a5e6cce69
Devan Kestel <devan.kestel@gmail.com>
2015-05-27 23:17:22 -0400

    Edited where.html.erb
    Template for rails, fee, student, and staff pages.

--- active_record_iron_roster/roster_server.rb
+++ active_record_iron_roster/roster_server.rb
@@ -3,10 +3,21 @@ require "./person"
 
 server = WEBrick::HTTPServer.new(:Port => 8000)
 @students = Person.all
+@selectors = {"rails" => {:subject => "ROR"}, "students" => {:student => true}, 
+              "staff" => {:student => false}, "fee" => {:subject => "FEE"}}
 
 server.mount_proc "/home" do |request, response|
   template = ERB.new(File.read "home.html.erb")
   response.body = template.result
+end
+
+@selectors.each do |page_name, where_query|
+  puts "#{page_name}"
+  server.mount_proc "/#{page_name}" do |request, response|
+    @where_query = where_query
+    template = ERB.new(File.read "where.html.erb")
+    response.body = template.result
+  end
 end
 
 server.mount_proc "/new_student_form" do |request, response|
