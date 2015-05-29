35ee8d45db409fe13b19adb4d14ad1063b8f66cb
Devan Kestel <devan.kestel@gmail.com>
2015-05-29 11:56:15 -0400

    Create files
    Created files copied from github. Completed setup.rb. Added empty
    methods to todo.rb as well as established db connection. Adding erb
    logic to index.html.er.

---
+++ todo_app_v1/todo.rb
@@ -0,0 +1,20 @@
+require 'active_record'
+
+class Todo
+  class Todo < ActiveRecord::Base
+    establish_connection adapter: 'sqlite3',
+                         database: 'todomvc.db'
+
+  #returns true if completed is true in database
+  def completed?
+  end
+
+  #returns true if edit link has been clicked
+  def editing?
+  end
+
+  #returns bool value based on whether or not there are any entries in the database
+  def self.has_todos?
+  end
+
+end
