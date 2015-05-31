31f16e46e1be0bcbb83df96b8ca79fc2d8c1f9a3
Devan Kestel <devan.kestel@gmail.com>
2015-05-30 23:37:48 -0400

    setup.rb

---
+++ terrible_todos/setup.rb
@@ -0,0 +1,12 @@
+require 'active_record'
+require './todo'
+
+ActiveRecord::Base.establish_connection adapter: 'sqlite3',
+                                        database: 'todomvc.db'
+
+ActiveRecord::Base.connection.create_table 'todos',
+  force: true do |table|
+    table.string :title
+    table.boolean :complete, default: false
+    table.boolean :edit, default: false
+  end
