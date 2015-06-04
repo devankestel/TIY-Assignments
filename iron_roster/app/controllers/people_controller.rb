3402f7ad333133e6b5e8c9d9547a7d38b2c57ce1
Devan Kestel <devan.kestel@gmail.com>
2015-06-04 00:34:54 -0400

    Updated Create New Student Functionality
    New students now able to post to DB and display on index page.

--- iron_roster/app/controllers/people_controller.rb
+++ iron_roster/app/controllers/people_controller.rb
@@ -1,4 +1,11 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
+  end
+  def new
+  end
+  def create
+    person = Person.create(name: params[:name], subject: params[:subject], github_account: params[:github_account])
+    redirect_to '/people'
   end
 end
