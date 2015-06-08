88e70938b7e0147ea8ae7db990e2e32148808c1d
Devan Kestel <devan.kestel@gmail.com>
2015-06-08 01:00:48 -0400

    Create new form and action
    Updated new.html.erb using form_for to send data for new blog creation
    along with author. Links author_id up and creates new author as well.
    Need to add logic to tell if an author is already in the database and
    respond appropriately.

--- first_blog/app/controllers/blogs_controller.rb
+++ first_blog/app/controllers/blogs_controller.rb
@@ -4,5 +4,12 @@ class BlogsController < ApplicationController
   def show
   end
   def new
+    @blog = Blog.new
+    @author = Author.new
+  end
+  def create
+    @author = Author.create(name: params[:author][:name])
+    @blog = Blog.create(title: params[:blog][:title], description: params[:blog][:description], author_id: @author.id)
+    redirect_to '/blogs'
   end
 end
