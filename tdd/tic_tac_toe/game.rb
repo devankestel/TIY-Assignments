472586d987f36a0d4cdca21cf4d134e8355abf29
Devan Kestel <devan.kestel@gmail.com>
2015-06-14 14:01:34 -0400

    Added Game methods #initialize, #start, #play.
    Still working on #play and need to do testing on all methods in game.rb.

--- tdd/tic_tac_toe/game.rb
+++ tdd/tic_tac_toe/game.rb
@@ -1,7 +1,42 @@
-#game is more of a script than a class
 
-#initialize Players and Board
+#require relevant class files
+require './player'
+require './board'
 
-player1 = Player.new
-player2 = Player.new
-board = Board.new
+class Game
+  attr_accessor :player1, :player2, :board
+
+  def initialize
+    #start new game
+    @player1 = Player.new
+    @player2 = Player.new
+    @board = Board.new
+    Player.set_mark_and_turn(@player1, @player2)
+  end 
+
+  def start
+    if @player1.turn == true
+      puts "Player 1 rolled a #{player1.roll} and Player 2 rolled a #{player2.roll}."
+      puts "Player 1 will go first and have mark 'X'."
+      puts "Player 2 will go second and have mark 'O'."
+    else
+      puts "Player 1 rolled a #{player1.roll} and Player 2 rolled a #{player2.roll}."
+      puts "Player 2 will go first and have mark 'X'."
+      puts "Player 1 will go second and have mark 'O'."
+    end
+  end
+
+  def play
+    if @player1.turn == true
+      current_player = @player1
+      name = "Player 1"
+    else
+      current_player = @player2
+      name = "Player 2"
+    end
+    puts "#{name}, it is your turn."
+    puts "#{board}"
+    puts "Please choose which row to add your mark: "
+    puts "Please choose which column to add your mark: "
+  end
+end #game
