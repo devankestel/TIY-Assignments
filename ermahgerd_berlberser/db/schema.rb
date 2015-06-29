6a7188ff58a1bef9f28e3aad80f862ace986eee0
Devan Kestel <devan.kestel@gmail.com>
2015-06-28 21:41:14 -0400

    Generated and migrated Ermahgerd model.
    With name, description, sprite, and number:integer columns.

--- ermahgerd_berlberser/db/schema.rb
+++ ermahgerd_berlberser/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628224440) do
+ActiveRecord::Schema.define(version: 20150629014021) do
+
+  create_table "ermahgerds", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "sprite"
+    t.integer  "number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "evolutions", force: :cascade do |t|
     t.string   "name"
