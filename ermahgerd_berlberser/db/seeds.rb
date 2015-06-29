4a444b0e7a67856052826fa81b23a8dfa6f50a2b
Devan Kestel <devan.kestel@gmail.com>
2015-06-28 20:36:31 -0400

    Wrote seed code to create Pokemon model entries.
    Tested on Pokemon 155 through 200.

--- ermahgerd_berlberser/db/seeds.rb
+++ ermahgerd_berlberser/db/seeds.rb
@@ -8,8 +8,23 @@
 
 pokemon_data = []
 pokemon_data[0] = nil
-(1..5).each do |number|
+(155..200).each do |number|
   pokemon_data[number] = HTTParty.get("http://pokeapi.co/api/v1/pokemon/#{number}/")
 end
 
-puts pokemon_data[1]["name"]
+puts pokemon_data[155]["name"]
+
+base_uri = "http://pokeapi.co"
+
+pokemon_data.each do |pokemon|
+  if pokemon #only run when array entry is not nil
+    pokemon_description = HTTParty.get(base_uri + pokemon["descriptions"][0]["resource_uri"])
+    pokemon_sprite = HTTParty.get(base_uri + pokemon["sprites"][0]["resource_uri"])
+    Pokemon.create(name: pokemon["name"], 
+                   number: pokemon["national_id"],
+                   description: pokemon_description["description"],
+                   sprite: base_uri + pokemon_sprite["image"])
+  end
+end
+
+puts Pokemon.find(1).description
